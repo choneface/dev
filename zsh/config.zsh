50c32663a8eb889bbbe5b56aa4bd0e42734065c2
choneface <gavin.garcia0001@gmail.com>
2026-01-30 20:17:36 -0500

    remove useless f() func

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,13 +1,4 @@
 # Custom zsh configuration
 # This file is sourced from ~/.zshrc
 
-# Quick cd to repos
-f() {
-    if [ -z "$1" ]; then
-        echo "Must provide a repository name"
-        return 1
-    fi
-    cd ~/Desktop/repos/$1
-}
-
 bindkey -s '^f' 'tmux-sessionizer\n'
